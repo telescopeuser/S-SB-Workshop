e707def708ae72ad7c1fc7366b6d4578ef572ba5
telescopeuser <telescopeuser@telescopeuser.com>
2019-12-30 17:01:34 +0800

    housekeeping

--- 00-R-In-Class/BondFunds_QQPlot.R
+++
@@ -1,8 +0,0 @@
-attach(BondFunds)
-
-## construct a q-q plot
-qqnorm(Return2009, col="blue", xlab="z Value", 
-       ylab="Return2009")
-qqline(Return2009, col="red", lwd=2)
-
-detach(BondFunds)
--- 00-R-In-Class/info.R
+++
@@ -1,3 +0,0 @@
-print("Welcome to NUS ISS")
-print("You are attending Statistics Bootcamp Using R")
-print(Sys.Date())
